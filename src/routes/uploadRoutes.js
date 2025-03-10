dcb2407b96420e01ff86e54eebd33934ca997b2e
Dxz23 <111670397+Dxz23@users.noreply.github.com>
2025-03-10 00:16:34 -0700

    Delete src directory

--- src/routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { uploadExcelAndSend } from '../controllers/uploadController.js';
-
-const router = express.Router();
-
-router.post('/upload', uploadExcelAndSend);
-
-export default router;
